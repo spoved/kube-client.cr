642a68dc3e0011d4bb68cff4de541fdb1c790e40
Holden Omans <holden.omans@gmail.com>
2019-04-17 10:40:12 -0400

    Added NONE to tls mode

--- src/kube/client/api.cr
+++ src/kube/client/api.cr
@@ -3,6 +3,19 @@ require "spoved/api/client"
 module Kube
   class Client
     class Api < Spoved::Api::Client
+      def initialize(
+        @host : String, @port : Int32? = nil,
+        @user : String? = nil, @pass : String? = nil,
+        logger : Logger? = nil,
+        @scheme = "https", @api_path = "api/v1",
+        tls_verify_mode = OpenSSL::SSL::VerifyMode::NONE
+      )
+        @tls_client = OpenSSL::SSL::Context::Client.new
+        @tls_client.verify_mode = tls_verify_mode
+        if logger
+          self.logger = logger
+        end
+      end
     end
   end
 end
