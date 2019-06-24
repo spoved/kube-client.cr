010d34e71ce3ffbbf8014da45f6aa3ed4cab6bf2
Holden Omans <holden.omans@gmail.com>
2019-06-24 13:56:33 -0400

    updates to use minikube

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,12 +1,28 @@
 require "spec"
 require "../src/kube-client"
 
-TEST_KUBE_CONFIG_FILE = "./spec/files/kube_config_cluster.yml"
-CONFIGURE_YAML        = YAML.parse(File.read(TEST_KUBE_CONFIG_FILE))
-CA_DATA               = Base64.decode_string(CONFIGURE_YAML["clusters"].as_a[0]["cluster"]["certificate-authority-data"].as_s)
-USER_CERT_DATA        = Base64.decode_string(CONFIGURE_YAML["users"].as_a[0]["user"]["client-certificate-data"].as_s)
-USER_KEY_DATA         = Base64.decode_string(CONFIGURE_YAML["users"].as_a[0]["user"]["client-key-data"].as_s)
-TEST_CLIENT           = Kube::Client.new(TEST_KUBE_CONFIG_FILE)
+TEST_KUBE_CONFIG_TEMPLATE = "./spec/files/kube_config_template.yml"
+TEST_KUBE_CONFIG_FILE     = "./spec/files/kube_config_test.yml"
+
+def gen_kube_config_file
+  temp = YAML.parse(File.read(TEST_KUBE_CONFIG_TEMPLATE))
+  temp["clusters"].as_a[0]["cluster"].as_h[YAML::Any.new("server")] = YAML::Any.new(ENV["KUBE_API_SERVER"])
+  temp["users"].as_a.each do |user|
+    user["user"].as_h[YAML::Any.new("token")] = YAML::Any.new(Base64.strict_encode(ENV["KUBE_TOKEN"]))
+  end
+  File.open(TEST_KUBE_CONFIG_FILE, "w+") do |file|
+    file.puts temp.to_yaml
+  end
+end
+
+gen_kube_config_file
+
+CA_DATA        = Base64.decode_string(CONFIGURE_YAML["clusters"].as_a[0]["cluster"]["certificate-authority-data"].as_s)
+USER_CERT_DATA = Base64.decode_string(CONFIGURE_YAML["users"].as_a[2]["user"]["client-certificate-data"].as_s)
+USER_KEY_DATA  = Base64.decode_string(CONFIGURE_YAML["users"].as_a[2]["user"]["client-key-data"].as_s)
+
+CONFIGURE_YAML = YAML.parse(File.read(TEST_KUBE_CONFIG_FILE))
+TEST_CLIENT    = Kube::Client.new(TEST_KUBE_CONFIG_FILE)
 
 module Kube
   class Client
--- test.sh
+++ test.sh
@@ -1,22 +1,33 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
+# minikube status
 
-minikube status
-
-kubectl config use-context minikube 
+kubectl config use-context minikube
 
 # Check all possible clusters, as you .KUBECONFIG may have multiple contexts:
-kubectl config view -o jsonpath='{"Cluster name\tServer\n"}{range .clusters[*]}{.name}{"\t"}{.cluster.server}{"\n"}{end}'
+# kubectl config view -o jsonpath='{"Cluster name\tServer\n"}{range .clusters[*]}{.name}{"\t"}{.cluster.server}{"\n"}{end}'
 
 # Select name of cluster you want to interact with from above output:
 export CLUSTER_NAME="minikube"
 
 # Point to the API server refering the cluster name
-APISERVER=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"${CLUSTER_NAME}\")].cluster.server}")
+export KUBE_API_SERVER=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"${CLUSTER_NAME}\")].cluster.server}")
 
 # Gets the token value
-TOKEN=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.token}"|base64 -d)
+export KUBE_TOKEN=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.token}"|base64 -d)
 
-# Explore the API with TOKEN
-curl -X GET ${APISERVER}/api --header "Authorization: Bearer ${TOKEN}" --insecure
+helm init 1>/dev/null 2>&1
+
+status=$(helm list --output json  2>/dev/null | jq '.Releases | .[] | select(.Name == "kubecr-test") | .Status' -r)
+if [[ -z "${status}" ]];then
+  helm install stable/percona --name kubecr-test 1>/dev/null 2>&1
+else
+  # helm del --purge kubecr-test  1>/dev/null 2>&1
+  # sleep 10
+  echo ""
+fi
+
+
+
+crystal spec
