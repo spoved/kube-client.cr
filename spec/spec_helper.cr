ccb8ff4e0cdf526c3f13b221feeb25759e310626
Holden Omans <holden.omans@gmail.com>
2019-06-25 09:07:23 -0400

    fix spec tests

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,6 +1,8 @@
 require "spec"
 require "../src/kube-client"
 
+# Spoved.logger.level = Logger::DEBUG
+
 TEST_KUBE_CONFIG_TEMPLATE = "./spec/files/kube_config_template.yml"
 TEST_KUBE_CONFIG_FILE     = "./spec/files/kube_config_test.yml"
 
@@ -8,7 +10,8 @@ def gen_kube_config_file
   temp = YAML.parse(File.read(TEST_KUBE_CONFIG_TEMPLATE))
   temp["clusters"].as_a[0]["cluster"].as_h[YAML::Any.new("server")] = YAML::Any.new(ENV["KUBE_API_SERVER"])
   temp["users"].as_a.each do |user|
-    user["user"].as_h[YAML::Any.new("token")] = YAML::Any.new(Base64.strict_encode(ENV["KUBE_TOKEN"]))
+    # user["user"].as_h[YAML::Any.new("token")] = YAML::Any.new(Base64.strict_encode(ENV["KUBE_TOKEN"]))
+    user["user"].as_h[YAML::Any.new("token")] = YAML::Any.new(ENV["KUBE_TOKEN"])
   end
   File.open(TEST_KUBE_CONFIG_FILE, "w+") do |file|
     file.puts temp.to_yaml
--- test.sh
+++ test.sh
@@ -1,33 +1,49 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
-# minikube status
-
-kubectl config use-context minikube
-
-# Check all possible clusters, as you .KUBECONFIG may have multiple contexts:
-# kubectl config view -o jsonpath='{"Cluster name\tServer\n"}{range .clusters[*]}{.name}{"\t"}{.cluster.server}{"\n"}{end}'
-
-# Select name of cluster you want to interact with from above output:
 export CLUSTER_NAME="minikube"
+export APP_NAME="kubecr-test"
+
+setup_minikube(){
+  mini_k_status=$(minikube status --format "{{.APIServer}}")
+
+  if [[ "${mini_k_status}" != "Running" ]]; then
+    minikube delete
+    minikube start --cpus 4 --memory 8192
+  fi
+
+  kubectl config use-context ${CLUSTER_NAME}
+}
+
+check_helm(){
+  set +e
+  helm list 1>/dev/null 2>&1
+  if [[ $? -ne 0 ]]; then
+    helm init 1>/dev/null 2>&1
+    sleep 10
+  fi
+  set -e
+
+  chart_status=$(helm list --output json 2>/dev/null | jq '.Releases | .[] | select(.Name == "kubecr-test") | .Status' -r)
+  if [[ -z "${chart_status}" ]];then
+    helm install stable/elastic-stack --name ${APP_NAME} -f ./spec/files/elastic-stack.yml 1>/dev/null 2>&1
+    sleep 60
+  fi
+}
+
+run_spec(){
+  crystal spec
+}
+
+
+setup_minikube
+check_helm
 
 # Point to the API server refering the cluster name
 export KUBE_API_SERVER=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"${CLUSTER_NAME}\")].cluster.server}")
 
 # Gets the token value
-export KUBE_TOKEN=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.token}"|base64 -d)
+export KUBE_TOKEN=$(kubectl get secrets -n kube-system -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.token}"|base64 -d)
 
-helm init 1>/dev/null 2>&1
-
-status=$(helm list --output json  2>/dev/null | jq '.Releases | .[] | select(.Name == "kubecr-test") | .Status' -r)
-if [[ -z "${status}" ]];then
-  helm install stable/percona --name kubecr-test 1>/dev/null 2>&1
-else
-  # helm del --purge kubecr-test  1>/dev/null 2>&1
-  # sleep 10
-  echo ""
-fi
-
-
-
-crystal spec
+# minikube dashboard
+run_spec
