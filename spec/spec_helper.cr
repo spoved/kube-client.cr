a435b0fe9e11d6d172fc3378c742993285f8ef75
Holden Omans <holden.omans@gmail.com>
2020-05-03 18:37:48 -0400

    update

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -12,21 +12,10 @@ TEST_KUBE_CONFIG_TEMPLATE = "./spec/files/kube_config_template.yml"
 TEST_KUBE_CONFIG_FILE     = "./spec/files/kube_config_test.yml"
 
 def gen_kube_config_file
-  unless ENV["KUBE_TOKEN"]?
-    ENV["KUBE_TOKEN"] = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ik1YYkdlT3BOckFJNTB2U240MFUtZzdKUnVzaWY4Yy1YRG53TzAtZUdfRFEifQ" \
-                        ".eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uY" \
-                        "W1lc3BhY2UiOiJrdWJlLXN5c3RlbSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJ" \
-                        "kZWZhdWx0LXRva2VuLThzcmI3Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQub" \
-                        "mFtZSI6ImRlZmF1bHQiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiJ" \
-                        "mYmEyYzM0NC0zYzExLTQ4MGUtYmFkMS1jYmM2M2I3ZTQ3ZGMiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6a" \
-                        "3ViZS1zeXN0ZW06ZGVmYXVsdCJ9.qZNlSnERXx4sAP9-5GF1glGprtrocTIhCRsNi-EX3qvwGIqnWVJV87U3OsQnkg" \
-                        "7cajVDOg0gpFiTW56lOFDhI8tJdwzfENqs51h_Zg_uO1c4A7QD8POKJP11yWFxWXsP3bAI-9XckKi5G8u1d5_4-cVn" \
-                        "H2RTa9FpM9yPBNoRDKEGJZ3Lwtkr-tSw-CjknhbsYvFs2BV8gsUKQCOyQop4U7HZLrKZjYqcdWG-_2n7oW97jEicVp" \
-                        "BZRAamuihbldfRTH8auhoaje76eg-pWfJTnm_WpriDCgO9ZvmKClz5JjOIMxM_BSzPXDvKLRQFqvJJiDK3BQxjOqVd" \
-                        "2m-40xZIlQ"
-  end
-
-  ENV["KUBE_API_SERVER"] = "https://192.168.64.4:8443" unless ENV["KUBE_API_SERVER"]?
+  raise "Missing: KUBE_TOKEN" unless ENV["KUBE_TOKEN"]?
+  raise "Missing: KUBE_API_SERVER" unless ENV["KUBE_API_SERVER"]?
+  raise "Missing: APP_NAME" unless ENV["APP_NAME"]?
+  raise "Missing: CLUSTER_NAME" unless ENV["CLUSTER_NAME"]?
 
   temp = YAML.parse(File.read(TEST_KUBE_CONFIG_TEMPLATE))
   temp["clusters"].as_a[0]["cluster"].as_h[YAML::Any.new("server")] = YAML::Any.new(ENV["KUBE_API_SERVER"])
