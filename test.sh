2fd1b396ee7a0e7504d2a6b5cdaa75db843e9476
Holden Omans <holden.omans@gmail.com>
2023-09-26 10:37:18 -0400

    small fixes

--- test.sh
+++ test.sh
@@ -8,5 +8,5 @@ jsonpath="{.clusters[?(@.name==\"k3d-${CLUSTER_NAME}\")].cluster.server}"
 export KUBE_API_SERVER=$(kubectl config view -o jsonpath="${jsonpath}")
 
 # Gets the token value
-export KUBE_TOKEN=$(kubectl get secrets -n kube-system -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.token}" | base64 -d)
+export KUBE_TOKEN=$(kubectl create token default)
 env | egrep "APP_NS|APP_NAME|CLUSTER_NAME|KUBE_" | sort >.env_test
